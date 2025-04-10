91c6c000527c194c1001e474229857ccc3be4585
Angelo Monteleone <144103148+AngeloMonteleone@users.noreply.github.com>
2025-04-10 12:11:17 +0200

    Add files via upload

---
+++ advection_Lax_Friedrichs.py
@@ -0,0 +1,80 @@
+#Exercise 1, Lax-Friedrichs method for advection equation
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+#Define parameters of the simulation
+a = 1.0
+L = 10
+J = 301
+c_f = 0.5
+
+#Lattice spacings
+dx = L/(J-1)
+dt = c_f * dx/a
+
+def advance(space_arr):
+    # ret = []
+    # for j in range(0,len(space_arr)):
+    #     ret.append(space_arr[j%J]-(a*dt/(2*dx))*(space_arr[(j+1)%J]-space_arr[(j-1)%J]))
+    ret = 0.5*(np.roll(space_arr,+1) + np.roll(space_arr,-1)) - (a*dt/(2*dx))*(np.roll(space_arr, +1) - np.roll(space_arr, -1))
+
+    return ret
+
+x = np.linspace(0,10,J)
+x0 = 0.5
+u_init = np.exp(-(x-x0)**2)
+u_old = u_init.copy()
+u_curr = []
+
+#create figure with a single subplot
+fig, ax = plt.subplots()
+plot, = ax.plot(x,u_init)
+ax.set_xlabel("x")
+ax.set_ylabel("u(x,t)")
+
+# for n in range(0,N):
+#     u_curr = advance(u_init)
+#     u_init = u_curr.copy()
+
+time = 0.0
+times = []
+norms = []
+def update(frame):
+    global u_curr,u_old,time
+
+    if(time > 20.0):
+        ani.event_source.stop()
+        #close previous plot
+        plt.close()
+        #plot initial and final state
+        plt.plot(x,u_init,label = "t=0")
+        plt.plot(x,u_curr,label = "t=20")
+        plt.title("Comparison between t=0 and t=20")
+        plt.xlabel("x")
+        plt.ylabel("u(x,t)")
+        plt.ylim(0,1.5)
+        plt.legend()
+        plt.savefig("final_Lax_Friedrichs.png")
+        #close the plot (otherwise animation crashes)
+        plt.close()
+        
+        plt.plot(times,norms)
+        plt.title("Evolution of the L2 norm")
+        plt.xlabel("t")
+        plt.ylabel("L2 norm of u(x,t)")
+        plt.savefig("norm_Lax_Friedrichs.png")
+        plt.close()
+
+    u_curr = advance(u_old)
+    u_old = u_curr.copy()
+
+    plot.set_ydata(u_curr)
+
+    time += dt
+    print("frame #{}\ttime: {}".format(frame+1,time))
+    times.append(time)
+    norms.append(np.linalg.norm(u_curr))
+    return plot,
+
+ani = animation.FuncAnimation(fig=fig, func=update, interval=10)
+plt.show()
